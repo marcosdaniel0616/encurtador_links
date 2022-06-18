cdd487d4860fb934ce5216f89a3df56d1a9574a8
Marcos Daniel <marcos.daniel0616@gmail.com>
2022-06-17 22:07:55 -0300

    Footer corrigido

--- encurtador_links/settings.py
+++ encurtador_links/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-on$kr4=(5bb9+#ri7g8em#713#mdu8p+z=0=3%@tdm%7qow+4p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
